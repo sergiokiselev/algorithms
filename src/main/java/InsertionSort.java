103b3762c8c95c85a8465ad1ecf4df1c2df37597
Sergey Kiselev <s.kiselyov@itransition.com>
2021-09-23 13:32:09 +0300

    Init commit

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        List<Integer> items = new Random().ints(100, 1, 200)
+                .boxed().collect(Collectors.toList());
+        System.out.println(items);
+
+        for (int i = 1; i < items.size(); i++) {
+            for (int j = i; j > 0; j--) {
+                if (items.get(j) > items.get(j - 1)) {
+                    Collections.swap(items, j, j - 1);
+                }
+            }
+        }
+
+        System.out.println(items);
+    }
+}
---
+++ src/main/java/InsertionSortDichotomy.java
@@ -0,0 +1,49 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+public class InsertionSortDichotomy {
+
+    public static void main(String[] args) {
+        List<Integer> items = ThreadLocalRandom.current().ints(100, 0, 200)
+                .boxed().collect(Collectors.toList());
+
+        System.out.println(items);
+
+        int l, r, m;
+        for (int i = 0; i < items.size(); i++) {
+            r = i;
+            l = 0;
+            while (r > l) {
+                m = (l + r) / 2;
+                if (items.get(m) > items.get(i)) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            System.out.println(r);
+            int x = items.get(i);
+            for (int j = i; j > r; j--) {
+                Collections.swap(items, j, j - 1);
+            }
+            items.set(r, x);
+        }
+        System.out.println(items);
+    }
+}
+
+//
+//100 99 98 97 96 95 95
+//
+//L = 0
+//R = 6
+//m = L + R / 2= 3
+//x = 95
+//if (a[m] > x) L = m else R = m
+//L = 4
+//R = 6
+//m = 5
+//if (a[5] > 95) L = 6 else R = 5
+//R = 5
